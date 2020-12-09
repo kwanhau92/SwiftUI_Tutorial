567ceb74ed228739516102c69cb0b90e7c357ff8
Ming kwan Hau <kwanhau92@MingdeAir.mshome.net>
2020-12-09 14:16:35 -0500

    created a Landmarks project

---
+++ Landmarks/Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Ming kwan Hau on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
