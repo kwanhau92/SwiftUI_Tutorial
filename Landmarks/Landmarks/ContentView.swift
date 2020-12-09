74b1fb8e0d009a8b4153bc4c775a7d48082a32b3
Ming kwan Hau <kwanhau92@MingdeAir.mshome.net>
2020-12-09 15:40:19 -0500

    creating and combinding views

--- Landmarks/Landmarks/ContentView.swift
+++ Landmarks/Landmarks/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            MapView()
+                .frame(height: 300)
+                .ignoresSafeArea(edges: .top)
+            CircleImage()
+                .offset(y: -130)
+                .padding(.bottom, -130)
+            VStack(alignment: .leading) {
+                Text("TurtleRock")
+                    .font(.title)
+                HStack {
+                    Text("Joshua Tree National Park")
+                    Spacer()
+                    Text("California")
+                }
+                .font(.subheadline)
+                .foregroundColor(.secondary)
+                
+                Divider()
+                Text("About Turtle Rock")
+                    .font(.title)
+                Text("Descriptive text goes here.")
+            }
+            
             .padding()
+            Spacer()
+        }
     }
 }
 
